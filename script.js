6651f019ca0b79e47d068205c5a557e3f7c6f4bf
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-13 22:22:50 -0300

    upgrade

---
+++ script.js
@@ -0,0 +1,33 @@
+let B7Validator = {
+  handleSubmit:(event) => {
+    event.preventDefault();
+    let send = true;
+
+    let inputs = form.querySelectorAll('input');
+
+    for(let i=0;i<inputs.length;i++) {
+      let input = inputs[i];
+      let check = B7Validator.checkInput(input);
+      if(check !== true) {
+        send = false;
+
+      }
+    }
+
+      send = false;
+    if(send) {
+      form.submit();
+    }
+  },
+  checkInput:(input) => {
+    let rules = input.getAttribute('data-rules');
+    if(rules !== null) {
+        
+    }
+
+    return true;
+  }
+}
+
+let form = document.querySelector('.b7validator');
+form.addEventListener('submit', B7Validator.handleSubmit);
