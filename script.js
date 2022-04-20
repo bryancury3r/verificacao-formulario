fafcade2c74a86d878445fb680e2962e01cc4df0
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-19 22:49:39 -0300

    scripts

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let B7Validator = {
 
     let inputs = form.querySelectorAll('input');
 
+    B7Validator.clearErros();
+
     for(let i=0;i<inputs.length;i++) {
       let input = inputs[i];
       let check = B7Validator.checkInput(input);
       if(check !== true) {
         send = false;
-        console.log(check);
+        B7Validator.showError(input, check);
       }
     }
 
@@ -33,7 +35,18 @@ let B7Validator = {
                   }
               break;
               case 'min':
-
+                  if(input.value.length < rDetails[1]) {
+                    return 'Campo tem que ter pelo menos '+rDetails[1]+' caractes'
+                  }
+              break;
+              case 'email':
+                  if(input.value != '') {
+                    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+                    ;
+                    if(!regex.test(input.value.toLowerCase())) {
+                      return 'E-mail digitado não é válido';
+                    }
+                  }
               break;
           }
 
@@ -41,8 +54,29 @@ let B7Validator = {
     }
 
     return true;
+  },
+  showError:(input, error) => {
+    input.style.borderColor = '#FF0000';
+
+    let errorElement = document.createElement('div');
+    errorElement.classList.add('error');
+    errorElement.innerHTML = error;
+
+    input.parentElement.insertBefore(errorElement, input.ElementSibling)
+  },
+  clearErros:() => {
+
+    let inputs = form.querySelectorAll('input');
+    for(let i = 0; i<inputs.length;i++) {
+      inputs[i].style = '';
+    }
+
+      let errorElements = document.querySelectorAll('.error');
+      for(let i = 0; i<errorElements.length;i++) {
+        errorElements[i].remove();
+      }
   }
-}
+};
 
 let form = document.querySelector('.b7validator');
 form.addEventListener('submit', B7Validator.handleSubmit);
